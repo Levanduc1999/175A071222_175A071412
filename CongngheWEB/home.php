0c72f256025000cebcc1b438f6117b654288d3b3
NgoHai16 <hainv720@wru.vn>
2019-12-27 11:46:23 +0700

    first commit

---
+++ CongngheWEB/home.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/fontawesome/css/all.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Trang chủ</title>
+</head>
+<body>
+    <header>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 logo">
+                <img src="images/logo.png" alt="logo">
+            </div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3 header-right">
+                <a href="#">Login</a>
+                      |
+                <a href="#"><img src="images/vi.jpg" alt="VN"></a>
+            </div>
+        </div>
+    </div>
+    </header>
+    <main>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2 menu">
+                    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                        <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#" role="tab" aria-controls="v-pills-home" aria-selected="true">Trang chủ</a>
+                        <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#" role="tab" aria-controls="v-pills-profile" aria-selected="false">Thông báo</a>
+                        <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#" role="tab" aria-controls="v-pills-messages" aria-selected="false">Giảng Viên</a>
+                        <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#" role="tab" aria-controls="v-pills-settings" aria-selected="false">Thông tin liên hệ</a>
+                    </div>
+                </div>
+                <div class="col-md-1"></div>
+                <div class="col-md-9">
+                   <h2>Thông Báo</h2>
+                   <div class="New">
+                        <div class="row content">
+                            <div class="col-sm-3">
+                                <img src="images/thong-bao.png" alt="1" width=190px>
+                            </div>
+                            <div class="col-sm-9">
+                                <div>
+                                    <h4>
+                                        <a href="#">	
+                                        Hội thảo chương trình Meister School đào tạo kỹ sư làm việc tại Nhật Bản
+                                        </a>
+                                    </h4>
+                                    <a href="#"><i class="fa fa-angle-right">Xem chi tiết</i></a>
+                                </div>                              
+                            </div>
+                        </div>
+                   </div>
+
+                   <div class="New">
+                        <div class="row content">
+                            <div class="col-sm-3">
+                                <img src="images/thong-bao.png" alt="1" width=190px>
+                            </div>
+                            <div class="col-sm-9">
+                                <div>
+                                    <h4>
+                                        <a href="#">	
+                                        Thông báo thi Olympic môn Sức bền vật liệu và Cơ học kết cấu cấp trường năm học 2019 -202
+                                        </a>
+                                    </h4>
+                                    <a href="#"><i class="fa fa-angle-right">Xem chi tiết</i></a>
+                                </div>                              
+                            </div>
+                        </div>
+                   </div>
+
+                   <div class="New">
+                        <div class="row content">
+                            <div class="col-sm-3">
+                                <img src="images/thong-bao.png" alt="1" width=190px>
+                            </div>
+                            <div class="col-sm-9">
+                                <div>
+                                    <h4>
+                                        <a href="#">	
+                                        Danh sách sinh viên nhận học bổng khuyến khích học tập kỳ II năm học 2018-2019
+                                        </a>
+                                    </h4>
+                                    <a href="#"><i class="fa fa-angle-right">Xem chi tiết</i></a>
+                                </div>                              
+                            </div>
+                        </div>
+                   </div>
+                   <div class="New">
+                        <div class="row content">
+                            <div class="col-sm-3">
+                                <img src="images/thong-bao.png" alt="1" width=190px>
+                            </div>
+                            <div class="col-sm-9">
+                                <div>
+                                    <h4>
+                                        <a href="#">	
+                                        Một số thông báo phục vụ sinh hoạt lớp tháng 12/2019
+                                        </a>
+                                    </h4>
+                                    <a href="#"><i class="fa fa-angle-right">Xem chi tiết</i></a>
+                                </div>                              
+                            </div>
+                        </div>
+                   </div>
+                   
+                   <div class="btn-group mr-2 btn" role="group" aria-label="First group">
+                        <button type="button" class="btn btn-secondary">1</button>
+                        <button type="button" class="btn btn-secondary">2</button>
+                        <button type="button" class="btn btn-secondary">3</button>
+                        <button type="button" class="btn btn-secondary">4</button>
+                    </div>
+                </div>
+    </main>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 f-left">
+                    <h3>© 2019 - TRƯỜNG ĐẠI HỌC THỦY LỢI</h3>
+                </div>
+                
+                <div class="col-md-6 f-right">
+                    <h4>TRƯỜNG ĐẠI HỌC THỦY LỢI - CS1</h4>
+                    <p>Địa chỉ : 175 TÂY SƠN, ĐỐNG ĐA, HÀ NỘI.</p>
+                    <p>Điện thoại: (024) 3852 2201 - Fax: (024) 3563 3351</p>
+                    <p>Email: phonghcth@tlu.edu.vn</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
